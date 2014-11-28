9492ecf96e9e01d7dbe2a070d663250ae726971f
hagai <hagai26@gmail.com>
2014-11-28 10:36:16 +0200

    Fix TCGA samples csv fix minfi baseDir (not used on prod, will be fixed on dev)

--- R/epibio/TCGA_aggregator.R
+++ R/epibio/TCGA_aggregator.R
@@ -2,7 +2,7 @@
 require(minfi)
 
 
-work_on_targets <- function(targets) {
+work_on_targets <- function(targets, baseDir) {
   study_levels <- levels(factor(targets$Study))
   type_levels <- levels(factor(targets$Type))
   
@@ -28,7 +28,7 @@ work_on_targets <- function(targets) {
         
         kind_name <- paste0(study, ".", type)
         kind_output_filename <- paste0(gsub(" ","_", kind_name , fixed=TRUE), ".csv")
-        full_filename <- file.path(TCGA_folder, kind_output_filename)
+        full_filename <- file.path(new_TCGA_folder, kind_output_filename)
         write.csv(cbind(CpG=rownames(level_data), level_data), file = full_filename, row.names=FALSE)
       }
     }
@@ -36,17 +36,18 @@ work_on_targets <- function(targets) {
 }
 
 # folders
-data_folder <- "..\\..\\data"
-generated_folder  <- file.path(data_folder, "generated")
-TCGA_folder <- file.path(generated_folder, "TCGA")
-idat_folder <- file.path(data_folder, "static\\L3_data\\idat")
-dir.create(TCGA_folder, recursive=TRUE, showWarnings=FALSE)
+data_folder <- file.path("..", "..", "data_small")
+generated_folder  <- file.path("..", "..", "generated")
+new_TCGA_folder <- file.path(generated_folder, "TCGA")
+idat_folder <- file.path(data_folder, "TCGA_L1", "450K_idat")
+dir.create(new_TCGA_folder, recursive=TRUE, showWarnings=FALSE)
 
 # targets
 targets <- read.csv(file.path(idat_folder, "samples.csv"), stringsAsFactors = FALSE)
 targets$Basename <- file.path(idat_folder, paste(targets$Sentrix.ID, targets$Sentrix.position, sep="_"))
 splited_targets <- split(targets,list(targets$Study, targets$Type), drop=TRUE)
-splited_targets <- lapply(splited_targets, FUN=function(x) head(x, 30))  #XXX
+splited_targets <- lapply(splited_targets, FUN=function(x) head(x, 20))  #XXX
+
 all_kinds = data.frame(number = sapply(splited_targets, FUN=nrow))
 print("Reading all kinds:")
 print(all_kinds)
@@ -54,8 +55,8 @@ print("")
 chunk_size <- 3
 chunked_targets <- split(splited_targets, ceiling(seq_along(splited_targets)/chunk_size))
 grouped_chunked_targets <- lapply(chunked_targets, function(x) do.call("rbind", x))
-ret <- lapply(grouped_chunked_targets, FUN=work_on_targets)
+ret <- lapply(grouped_chunked_targets, FUN=work_on_targets, NULL)
 
 all_kinds_filename = file.path(generated_folder, 'TCGA_all_kinds.csv')
 write.csv(cbind(kind=rownames(all_kinds), all_kinds), file = all_kinds_filename, row.names=FALSE)
-
+print("DONE")
