6198d531dc7cc35ca7ce912a6672796b3694469d
hagai <hagai26@gmail.com>
2015-02-03 22:26:44 +0200

    add progress

--- R/epibio/organize_tcga.R
+++ R/epibio/organize_tcga.R
@@ -6,7 +6,7 @@ source("common.R")
 
 work_on_targets <- function(targets, idat_folder, tcga_inside_name) {
   type <- targets$histological_type[[1]]
-  study <- targets$sample_type[[1]]  
+  study <- targets$sample_type[[1]]
   output_filename <- get_output_filename(generated_TCGA_folder, tcga_inside_name, study, type)
   if(file.exists(output_filename)) {
     print(sprintf('%s already exists. skipping', basename(output_filename)))
@@ -33,6 +33,7 @@ work_on_tcga_folder <- function(tcga_inside_name, tcga_folder) {
                         na.strings=c("NA", "0"), quote="\"", stringsAsFactors=FALSE)
   targets$barcode <- targets$Array.Data.File
   splited_targets <- split(targets, list(targets$histological_type, targets$sample_type), drop=TRUE)
+  print(sprintf('-> working on %s targets', length(splited_targets)))
   ret <- lapply(splited_targets, FUN=work_on_targets, idat_folder, tcga_inside_name)
 }
 
@@ -42,6 +43,11 @@ dir.create(generated_TCGA_folder, recursive=TRUE, showWarnings=FALSE)
 tcga_folder <- file.path(external_disk_data_path, 'TCGA')
 tcga_inside_folders <- list.files(tcga_folder)
 tcga_inside_folders <- head(tcga_inside_folders, 5) #XXX
-lapply(tcga_inside_folders, FUN=work_on_tcga_folder, tcga_folder)
+for (i in seq_along(tcga_inside_folders)) {
+  cur <- tcga_inside_folders[[i]]
+  print(sprintf('working on %s (%d/%d)', cur, i, length(tcga_inside_folders)))
+  work_on_tcga_folder(cur, tcga_folder)
+}
+
 
 print("DONE")
