2b535e1cd3f52589bce759cd47b8701664cb1eb7
hagai26 <hagai26@gmail.com>
2015-06-21 13:26:35 +0300

    cosmetics

--- R/epibio/organize_lab_data.R
+++ R/epibio/organize_lab_data.R
@@ -16,7 +16,7 @@ betas.table <- process_rnb_set_to_betas(rnb.set, TRUE)
 pheno <- pheno(rnb.set)
 sample_groups <- unique(pheno$Sample_Group)
 
-dir.create(generated_lab_data_folder, showWarnings=TRUE)
+dir.create(generated_lab_data_folder, recursive=TRUE, showWarnings=FALSE)
 
 for (i in 1:length(sample_groups)) {
 	cols <- which(pheno$Sample_Group == sample_groups[i])
