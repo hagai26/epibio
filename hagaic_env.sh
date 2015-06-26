8f0591fa48ca88344d8ff0553dc258260323eb7f
hagai26 <hagai26@gmail.com>
2015-06-26 09:40:27 +0300

    update paths

--- hagaic_env.sh
+++ hagaic_env.sh
@@ -1,3 +1,3 @@
 setenv EDITOR vim
-source /cs/icore/joshua.moss/dor/hagaic/epibio/R/epibio/r_env
+source /cs/icore/joshua.moss/dor/hagaic/epibio/R/scripts/r_env
 
--- hagaic_env_3.2.1.sh
+++ hagaic_env_3.2.1.sh
@@ -1,3 +1,3 @@
 setenv EDITOR vim
-source /cs/icore/joshua.moss/dor/hagaic/epibio/R/epibio/r_env_3.2.1
+source /cs/icore/joshua.moss/dor/hagaic/epibio/R/scripts/r_env_3.2.1
 
