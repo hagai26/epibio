7ffd7f31e044ce8214ce3303e53bc5b88ff973ef
hagai26 <hagai26@gmail.com>
2015-07-21 10:20:39 +0300

    moved organized to new folder

--- R/epibio/config.R
+++ R/epibio/config.R
@@ -1,11 +1,13 @@
 
 
 generated_folder  <- file.path("..", "..", "generated")
-generated_TCGA_folder <- file.path(generated_folder, "TCGA")
-generated_GEO_folder <- file.path(generated_folder, "GEO")
-generated_lab_data_folder <- file.path(generated_folder, "lab_data")
+organized_folder <- file.path(generated_folder, 'organized')
 generated_merged_folder <- file.path(generated_folder, "merged")
 summary_folder <- file.path(generated_folder, 'summary')
+
+generated_TCGA_folder <- file.path(organized_folder, "TCGA")
+generated_GEO_folder <- file.path(organized_folder, "GEO")
+generated_lab_data_folder <- file.path(organized_folder, "lab_data")
 
 data_folder <- file.path("..", "..", "data")
 external_disk_data_path <- file.path('../../../my_atlas')
