d1d3873410d38d63e81658862fad51161582c3cb
hagai26 <hagai26@gmail.com>
2015-07-21 22:28:44 +0300

    save as csv

--- R/epibio/summary_files_smaller.R
+++ R/epibio/summary_files_smaller.R
@@ -8,19 +8,28 @@ source("config.R")
 source("common.R")
 
 
-make_summary_smaller <- function(folder_name) {
+make_summary_smaller <- function(folder_name, good_blood_rows) {
   print(sprintf('- Making %s smaller -', folder_name))
   input_folder <- file.path(summary_folder, folder_name)
   output_folder <- file.path(summary_folder, folder_name)
   
   csv_filename <- file.path(input_folder, 'means.txt')
   data <- read.table(csv_filename, sep='\t', header = TRUE)
-  data[,-1] <- round(data[,-1], 2) # the "-1" excludes column 1
-  write.csv(data, file.path(output_folder, 'means_small.csv'), row.names=FALSE, quote=FALSE)
+  old_row_num <- nrow(data)
+  if(is.null(good_blood_rows)) {
+    good_blood_rows <- data[!is.na(data$healthy.whole_blood) & data$healthy.whole_blood>=0.7,]$rn
+  }
+  striped_data <- data[good_blood_rows,]
+  print(sprintf('new nrow=%d, old nrow=%d', nrow(striped_data), old_row_num))
+  striped_data[,-1] <- round(striped_data[,-1], 2) # the "-1" excludes column 1
+  write.csv(striped_data, file.path(output_folder, 'means_small.csv'), 
+            row.names=FALSE, quote=FALSE)
+  good_blood_rows
 }
 
-make_summary_smaller('GEO')
-make_summary_smaller('TCGA')
-make_summary_smaller('lab_data')
+# we use GEO to get good_blood_rows
+good_blood_rows <- make_summary_smaller('GEO', NULL)
+make_summary_smaller('TCGA', good_blood_rows)
+make_summary_smaller('lab_data', good_blood_rows)
 
 print("DONE")
